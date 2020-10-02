20c9b56801a3fde69eae1667d1da9556b545664a
converse98 <leandroandre1538@gmail.com>
2020-10-02 00:18:28 -0500

    footer semantico

--- beurer_plantilla/src/includes/footer.php
+++ beurer_plantilla/src/includes/footer.php
@@ -1,73 +1,65 @@
 <br> 
 <div class="wrapper-footer" id="piedepag">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-4 col-1-5 visible-lg">
-                    <h2 class="titles-footer">Productos</h2>
-                    <ul>
-                        <li><a href="salud" class="link-footer">Salud</a></li>
-                        <li><a href="bienestar" class="link-footer">Bienestar</a></li>
-                        <li><a href="belleza" class="link-footer">Belleza</a></li>
-                        <li><a href="actividad" class="link-footer">Actividad</a></li>
-                        <li><a href="linea-bebe" class="link-footer">Línea de bebé</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-4 col-1-5 visible-lg">
-                    <h2 class="titles-footer">ayuda al cliente</h2>
-                    <ul>
-                        <li><a href="preguntas-frecuentes" class="link-footer">FAQ</a></li>
-                        <li><a href="reclamos.php" class="link-footer">Libro de Reclamaciones</a></li> 
-                        <li><a href="centro-de-descargas" class="link-footer">Centro de descargas</a></li>
-                        <li><a href="terminos-y-condiciones" class="link-footer">Términos y condiciones</a></li>
-                        <li><a href="politicas-de-privacidad" class="link-footer">Políticas y privacidad</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-4 col-1-5 visible-lg">
-                    <h2 class="titles-footer"><a href="reclamos.php">Seguimiento de Pedido</a></h2>
-                    <h2 class="titles-footer"><a href="nosotros">quiénes somos</a></h2>
-                    <h2 class="titles-footer"><a href="contactanos">contacto</a></h2>
-                    
-                </div>
-                <div class="col-sm-5 col-md-6 col-1-5 phons-f">
-                    <!-- <h2 class="phones-footer"><a href="#"><i class="icon-f icon-phone"></i> (01) 978 440 034</a></h2 class="phones-footer"> -->
-
-                                                                        <h2 class="phones-footer"><a href="tel:(01) 978 440 034"><i class="icon-f icon-phone"></i>  (01) 978 440 034</a></h2>
-                                                    <h2 class="phones-footer"><a href="tel:(01) 255 3738"><i class="icon-f icon-phone"></i>  (01) 255 3738</a></h2>
-                            
-                    
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-4 col-1-5 visible-lg">
+                <h2 class="titles-footer">Productos</h2>
+                <ul>
+                    <li><a href="salud" class="link-footer">Salud</a></li>
+                    <li><a href="bienestar" class="link-footer">Bienestar</a></li>
+                    <li><a href="belleza" class="link-footer">Belleza</a></li>
+                    <li><a href="actividad" class="link-footer">Actividad</a></li>
+                    <li><a href="linea-bebe" class="link-footer">Línea de bebé</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-4 col-1-5 visible-lg">
+                <h2 class="titles-footer">ayuda al cliente</h2>
+                <ul>
+                    <li><a href="preguntas-frecuentes" class="link-footer">FAQ</a></li>
+                    <li><a href="reclamos.php" class="link-footer">Libro de Reclamaciones</a></li> 
+                    <li><a href="centro-de-descargas" class="link-footer">Centro de descargas</a></li>
+                    <li><a href="terminos-y-condiciones" class="link-footer">Términos y condiciones</a></li>
+                    <li><a href="politicas-de-privacidad" class="link-footer">Políticas y privacidad</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-4 col-1-5 visible-lg">
+                <h2 class="titles-footer"><a href="reclamos.php">Seguimiento de Pedido</a></h2>
+                <h2 class="titles-footer"><a href="nosotros">quiénes somos</a></h2>
+                <h2 class="titles-footer"><a href="contactanos">contacto</a></h2>        
+            </div>
+            <div class="col-sm-5 col-md-6 col-1-5 phons-f">
+                <h2 class="phones-footer"><a href="tel:(01) 978 440 034"><i class="icon-f icon-phone"></i>  (01) 978 440 034</a></h2>
+                <h2 class="phones-footer"><a href="tel:(01) 255 3738"><i class="icon-f icon-phone"></i>  (01) 255 3738</a></h2>   
                     <!-- EMAIL -->
-                                                                        <a href="mailto:ventas@beurer.pe" class="link-footer d-block"><i class="icon-f icon-sobre"></i>  ventas@beurer.pe</a>
-                                            
+                    <a href="mailto:ventas@beurer.pe" class="link-footer d-block"><i class="icon-f icon-sobre"></i>  ventas@beurer.pe</a>
                     <!-- DIRECCIÓN -->
-                                                                        <a href="#" class="link-footer d-block"><i class="icon-f icon-ubc"></i>  Av. Caminos del Inca Nº 257 Tienda Nº 149 Santiago de Surco - Lima</a>
-                                            
+                    <a href="#" class="link-footer d-block"><i class="icon-f icon-ubc"></i>  Av. Caminos del Inca Nº 257 Tienda Nº 149 Santiago de Surco - Lima</a>                        
                     <div class="rs-f hidden-md hidden-lg">
                         <a href="https://www.facebook.com/beurerperu/" target="_blank" class="icon-rs-f icon-facebook"></a>
                         <a href="https://instagram.com/beurerperu?igshid=vnrvppz3nm6f" target="_blank" class="icon-rs-f icon-instagram"></a>
                         <a href="https://www.youtube.com/channel/UCORqBGCGe3NOrllUAal4COg" target="_blank" class="icon-rs-f icon-youtube"></a>
                     </div>
-                </div>
-                <div class="col-sm-7 col-md-6 col-1-6 wrapper-susc">
-                    <h2 class="titles-footer hidden-xs hidden-sm">suscríbete</h2>
-                    <p class="link-footer hidden-xs">Recibe actualizaciones por correo eléctronico sobre nuestra tienda y ofertas especiales.</p>
-                    <p class="link-footer link-f-mob visible-xs">Recibe nuestras ofertas especiales.</p>
-                    <form id="subscribe" method="POST" action="subscribe/send" title="Suscrito con exito" data-placement="top">
-                        <div class="content-btn-susc">
-                            <input type="email" name="subscribe" class="input-susc">
+            </div>
+            <div class="col-sm-7 col-md-6 col-1-6 wrapper-susc">
+                <h2 class="titles-footer hidden-xs hidden-sm">suscríbete</h2>
+                <p class="link-footer hidden-xs">Recibe actualizaciones por correo eléctronico sobre nuestra tienda y ofertas especiales.</p>
+                <p class="link-footer link-f-mob visible-xs">Recibe nuestras ofertas especiales.</p>
+                <form id="subscribe" method="POST" action="subscribe/send" title="Suscrito con exito" data-placement="top">
+                    <div class="content-btn-susc">
+                        <input type="email" name="subscribe" class="input-susc">
                             <button type="submit" class="btn-susc">SUSCRÍBETE</button>
-                        </div>
+                    </div>
                         <div class="col-xs-12 px-0">
                             <div class="checkbox">
                                 <label class="font-light label-pol">
-                                    <input type="checkbox"  required /><i class="helper"></i><span>He leído y acepto
-                                        las <a href="politicas-de-privacidad" class="span-pol color-primary btn-modals">Políticas de Privacidad</a></span>
+                                    <input type="checkbox"  required /><i class="helper"></i><span>He leído y acepto las <a href="politicas-de-privacidad" class="span-pol color-primary btn-modals">Políticas de Privacidad</a></span>
                                 </label>
                             </div>
                         </div>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
+    </div>
         <!--CREDITOS -->
         <div class="crd">
             <div class="container">
@@ -88,14 +80,13 @@
                 </div>
             </div>
         </div>
-    </footer>
 </div>
 
 <div class="popup-ini" id="login" style="display: none;">
-          <div class="popup-inner">
-           <img id="img-popup" src="https://beurer.pe/assets/sources/CANALES DE ATENCION-02.jpg" class="img-responsive" alt="">
-            <button type="button" class="close"><span aria-hidden="true">×</span></button>
-          </div>
+    <div class="popup-inner">
+        <img id="img-popup" src="https://beurer.pe/assets/sources/CANALES DE ATENCION-02.jpg" class="img-responsive" alt="">
+        <button type="button" class="close"><span aria-hidden="true">×</span></button>
+    </div>
 </div>		
 
 
@@ -104,7 +95,6 @@
 
 <script src="assets/js/libraries/animate-it.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-<!--script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script-->
 <script src="https://cdn.rawgit.com/igorlino/elevatezoom-plus/1.1.6/src/jquery.ez-plus.js"></script>
 <script src="assets/js/libraries/fancybox.min.js"></script>
 <script src="assets/js/libraries/fullpage.js"></script>
