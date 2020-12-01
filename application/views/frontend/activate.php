cc78158ec56c7a7e661413107fab73ef01e36f52
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-01 18:24:47 -0500

    fix

---
+++ application/views/frontend/activate.php
@@ -0,0 +1,15 @@
+<?php include 'src/includes/header.php' ?>
+
+<div class="content-recovery">
+    <div class="row-content-recovery">
+        <h2>Cuenta activada correctamente</h2>
+        <p>se redirigira a la pagina principal para que inicie sesión.</p>
+        <hr>
+    </div>
+</div>
+
+<?php include 'src/includes/footer.php' ?>
+
+</body>
+
+</html>
