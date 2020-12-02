fbbf4a0d73b1cce784bb17b9855632c662c5f41c
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-01 19:07:24 -0500

    fix

--- application/views/mail/confirm_register.php
+++ application/views/mail/confirm_register.php
@@ -22,7 +22,7 @@
                         <td align="center" style="padding: 14px; position: relative; background-size: 100% 100%;" background="<?php echo base_url('assets/images/fondomail.png'); ?>">
                             <!-- <img width="95%" src="<?php echo base_url('assets/images/fondomail.png'); ?>" alt="#" style="border-radius: 14px;"> -->
                             <span style="padding:14px 25px;font-size:45px;position: absolute;top:40px;left:30px; color: #fff;text-align: left">
-                                <span style="display: block;">Bienvenido a beurer.pe</span> 
+                                <span style="display: block;">Bienvenido a beurer</span>.<span style="display: block;">pe</span> 
                                 <span style="display: block; font-size: 25px;margin-top:20px">Tu Cuenta se ha registrado correctamente</span>
                             </span>
 
