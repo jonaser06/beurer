35a52b8078d5d22e0a129b7b2bf170bf827bb693
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-16 01:43:10 -0500

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-    $config['base_url'] = 'https://www.blogingenieria.site/';
+    $config['base_url'] = 'https://beurer.pe/';
     // $config['base_url'] = 'http://localhost/beurer/'; 
 /*
   |--------------------------------------------------------------------------
