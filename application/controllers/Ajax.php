278825661f8c3bb06a048481b236412ffd1df761
renzo <renzoedward23@gmail.com>
2021-02-03 11:32:53 -0500

    add

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -164,6 +164,7 @@ class Ajax extends MY_Controller
         
         $salida .= '<tr>';
         $salida .= '<td>Código de Compra</td>';
+        $salida .= '<td>Modelo</td>';
         $salida .= '<td>Nombres</td>';
         $salida .= '<td>Telefono</td>';
         $salida .= '<td>Correo</td>';
