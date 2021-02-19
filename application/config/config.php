5f40a3cdaa84eca5475e57a8deb555460da8533c
renzo <renzoedward23@gmail.com>
2021-02-19 16:46:15 -0500

    add

--- application/config/config.php
+++ application/config/config.php
@@ -26,6 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
     $config['base_url'] = 'https://beurer.pe/';
     
+    
     // $config['base_url'] = 'http://localhost/beurer/';
 /*
   |--------------------------------------------------------------------------
